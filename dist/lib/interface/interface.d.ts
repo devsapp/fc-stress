f0ad4a4f55b8f439d3d1c162794734402f6f7c98
千风 <qianfeng.zqf@alibaba-inc.com>
2021-09-24 15:31:51 +0800

    version 0.0.8

---
+++ dist/lib/interface/interface.d.ts
@@ -0,0 +1,18 @@
+export interface StressOption {
+    numUser?: number;
+    spawnRate?: number;
+    runningTime?: number;
+    functionType: string;
+    invocationType?: string;
+}
+export interface HttpTypeOption {
+    url: number;
+    method?: string;
+    body?: any;
+}
+export interface EventTypeOption {
+    serviceName: string;
+    functionName: string;
+    qualifier?: string;
+    payload?: any;
+}
